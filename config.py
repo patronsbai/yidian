4ab34733d16c5ccfb557e98d7bc1e0e4801357d9
baixiaobai <18222083687@163.com>
2018-03-29 17:50:12 +0800

    add config.py content

---
+++ config.py
@@ -0,0 +1,22 @@
+# -*- coding:utf-8 -*-
+
+import redis
+
+
+
+
+class Config(object):
+
+    DEBUG=True
+    SECRET_KEY = "sdgvsdvsdvsdvsv"
+
+    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:33006/ihome18"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    REDIS_HOST = "127.0.0.1"
+    REDIS_PORT = 6379
+
+    SESSION_TYPE = "redis"
+    SESSION_REDIS = redis.StrictRedis(REDIS_HOST, REDIS_PORT)
+    SESSION_USE_SIGNER = True
+    PERMANENT_SESSION_LIFETIME = 86400
--- manager.py
+++ manager.py
@@ -7,23 +7,9 @@ from flask_wtf.csrf import CSRFProtect
 from flask_session import Session
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
+from config import Config
 
 
-class Config(object):
-
-    DEBUG=True
-    SECRET_KEY = "sdgvsdvsdvsdvsv"
-
-    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:33006/ihome18"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    REDIS_HOST = "127.0.0.1"
-    REDIS_PORT = 6379
-
-    SESSION_TYPE = "redis"
-    SESSION_REDIS = redis.StrictRedis(REDIS_HOST, REDIS_PORT)
-    SESSION_USE_SIGNER = True
-    PERMANENT_SESSION_LIFETIME = 86400
 
 app = Flask(__name__)
 app.config.from_object(Config)
