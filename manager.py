5390de5a89034e3b8a1188374d7fccb7f5a0e5e6
baixiaobai <18222083687@163.com>
2018-03-29 18:48:06 +0800

    complete config

--- manager.py
+++ manager.py
@@ -2,9 +2,10 @@
 
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
-from iHome import db, redis_store, create_app
+from iHome import db, create_app
 
 app = create_app("development")
+from iHome import  redis_store
 manager = Manager(app)
 Migrate(app, db)
 manager.add_command("db", MigrateCommand)
