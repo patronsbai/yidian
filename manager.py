72bd1e0994cb73f96acfc306c901b8f7fa5ed79b
baixiaobai <18222083687@163.com>
2018-03-29 17:58:43 +0800

    add dir iHome package

--- manager.py
+++ manager.py
@@ -1,22 +1,10 @@
 # -*- coding:utf-8 -*-
 
-import redis
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_wtf.csrf import CSRFProtect
-from flask_session import Session
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
-from config import Config
+from iHome import db, app, redis_store
 
 
-
-app = Flask(__name__)
-app.config.from_object(Config)
-db = SQLAlchemy(app)
-redis_store = redis.StrictRedis(Config.REDIS_HOST, Config.REDIS_PORT)
-csrf = CSRFProtect(app)
-Session(app)
 manager = Manager(app)
 Migrate(app, db)
 manager.add_command("db", MigrateCommand)
